5aa427b7ad349ec7372d9b3ba3de0211a5f337a8
osenoah <noahoshose2@gmail.com>
2022-12-14 06:39:16 -0800

    Add two integers and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ *@x: print int x
+ *@y: print int y
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+return (x + y);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,42 +5,42 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
-		for (a = 0; a <= 9; a++)
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
 {
-		for (b = 0; b <= 9; b++)
+for (b = 0; b <= 9; b++)
 {
-	c = a * b;
-		if ((c / 10) == 0)
+c = a * b;
+if ((c / 10) == 0)
 {
-		if (b == 0)
+if (b == 0)
 {
-	_putchar ('0');
+_putchar ('0');
 }
-		if (b != 0)
+if (b != 0)
 {
-	_putchar (' ');
-	_putchar ((c % 10) + '0');
+_putchar (' ');
+_putchar ((c % 10) + '0');
 }
-		if (b < 9)
+if (b < 9)
 {
-	_putchar(',');
-	_putchar (' ');
-}
-}
-		else
-{
-	_putchar ((c / 10) + '0');
-	_putchar ((c % 10) + '0');
-		if (b < 9)
-{
-	_putchar(',');
-	_putchar (' ');
+_putchar(',');
+_putchar (' ');
 }
 }
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
 }
-	_putchar ('\n');
+}
+}
+_putchar ('\n');
 }
 }
