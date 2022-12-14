42c5339ccc5a86badb66b3278b17e1f45316c428
osenoah <noahoshose2@gmail.com>
2022-12-14 07:14:10 -0800

    Print  the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,9 @@
 /**
  *  * print_times_table - prints the times table
  *   * @n: integer for which the times table will be printed
+ *    *
  *     * Description: prints the times table
+ *      *
  *       * Return: void
  *        */
 
