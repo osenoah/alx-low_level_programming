4e65a78a78526b717a5e0f133fa29db9dab60e4a
osenoah <noahoshose2@gmail.com>
2022-12-22 01:05:06 -0800

    Print a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+/* int main(void) adds to the file */
 int main(void)
 {
 int n;
 int a[5];
 int *p;
+
 a[2] = 1024;
 p = &n;
   /*
@@ -14,7 +16,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-p[5] = 98;
+*(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
