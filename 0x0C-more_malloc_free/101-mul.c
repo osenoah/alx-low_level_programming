7e1a86f39e7c834aa4fd05ef993ca2eaf3c4f3bb
osenoah <noahoshose2@gmail.com>
2023-01-12 11:53:28 -0800

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,8 +1,12 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
+ *
  * Return: no return.
  */
 void _is_zero(char *argv[])
